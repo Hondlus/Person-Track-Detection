b5b90cf4b38fb5524fe21fb2cc5e2f9532e29f48
Hondlus <hldeshengyin@sina.com>
2024-11-01 17:29:42 +0800

    sixth commit

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 from collections import defaultdict
 import numpy as np
 import matplotlib.path as mplPath
+import codecs
 
 
 model = YOLO('./weights/yolo11n.pt')    ### Pre-trained weights
@@ -13,10 +14,10 @@ track_history = defaultdict(lambda: [])
 # Open the video file
 # video_path = "./test_video/test.mp4"
 # video_path = 0
-# video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/101" # 外走廊高清
-# video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/102" # 外走廊标清 640 360
+# video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/101" # 外走廊高清  1920 1080
+video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/102" # 外走廊标清 640 360
 # video_path = "rtsp://admin:HikNJQXFP@192.168.50.10:554/Streaming/Channels/102" # 屋内大屏摄像头
-video_path = "rtsp://admin:Dxw202409@192.168.50.20:554/stream2"  # 15fps 640 480
+# video_path = "rtsp://admin:Dxw202409@192.168.50.20:554/stream2"  # 15fps 640 480
 cap = cv2.VideoCapture(video_path)
 
 # Write the video file
@@ -27,48 +28,53 @@ output_video_path = "C:/Users/DXW/Desktop/yolo_track_reid/output_video/test.mp4"
 fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
 output_video = cv2.VideoWriter(output_video_path, fourcc, fps, (w, h), isColor=True)  # 创建一个VideoWriter对象用于写视频
 
-vh_down = []
-counter = []
-vh_up = []
-counter2 = []
-# 多边形坐标
-POLYGON_UP = np.array([
-    [220, 0], # 左上
-    [430, 0], # 右上
-    [430, 480], # 右下
-    [220, 480], # 左下
-])
-POLYGON_DOWN = np.array([
+vh_1 = []
+counter_1 = []
+vh_2 = []
+counter_2 = []
+# font_path = 'path/to/your/font.ttf'  # 替换为你的字体文件路径
+# font = cv2.freetype.createFreeType2(font_path)
+
+# 多边形坐标  高清 x:795、950 标清 x:260、320
+POLYGON_1 = np.array([
     [0, 0], # 左上
-    [210, 0], # 右上
-    [210, 480], # 右下
-    [0, 480], # 左下
+    [440, 0], # 右上
+    [440, 360], # 右下
+    [0, 360], # 左下
 ])
-POLYGON_DOWN2 = np.array([
-    [440, 0],  # 左上
-    [640, 0],  # 右上
-    [640, 480],  # 右下
-    [440, 480],  # 左下
+POLYGON_2 = np.array([
+    [460, 0], # 左上
+    [640, 0], # 右上
+    [640, 360], # 右下
+    [460, 360], # 左下
 ])
+# POLYGON_2_2 = np.array([
+#     [325, 0],  # 左上
+#     [640, 0],  # 右上
+#     [640, 360],  # 右下
+#     [325, 360],  # 左下
+# ])
 
 # 是否穿越区域
 def is_crossing(xc, yc):
     #####going DOWN#####
-    # mplPath.Path(POLYGON_UP).contains_point((xc, yc)判断中心点是否在区域内
-    if mplPath.Path(POLYGON_UP).contains_point((xc, yc)):
-        vh_down.append(track_id)
-    if track_id in vh_down:
-        if mplPath.Path(POLYGON_DOWN).contains_point((xc, yc)):
-            if counter.count(track_id) == 0:
-                counter.append(track_id)
+    # mplPath.Path(POLYGON_1).contains_point((xc, yc)判断中心点是否在区域内
+    if mplPath.Path(POLYGON_1).contains_point((xc, yc)):
+        vh_1.append(track_id)
+    if track_id in vh_1:
+        if mplPath.Path(POLYGON_2).contains_point((xc, yc)):
+        # if mplPath.Path(POLYGON_2).contains_point((xc, yc)) or mplPath.Path(POLYGON_2_2).contains_point((xc, yc)):
+            if counter_2.count(track_id) == 0:
+                counter_2.append(track_id)
 
     #####going UP#####
-    if mplPath.Path(POLYGON_DOWN).contains_point((xc, yc)) or mplPath.Path(POLYGON_DOWN2).contains_point((xc, yc)):
-        vh_up.append(track_id)
-    if track_id in vh_up:
-        if mplPath.Path(POLYGON_UP).contains_point((xc, yc)):
-            if counter2.count(track_id) == 0:
-                counter2.append(track_id)
+    if mplPath.Path(POLYGON_2).contains_point((xc, yc)):
+    # if mplPath.Path(POLYGON_2).contains_point((xc, yc)) or mplPath.Path(POLYGON_2_2).contains_point((xc, yc)):
+        vh_2.append(track_id)
+    if track_id in vh_2:
+        if mplPath.Path(POLYGON_1).contains_point((xc, yc)):
+            if counter_1.count(track_id) == 0:
+                counter_1.append(track_id)
 
 # tm = cv2.TickMeter()
 # Loop through the video frames
@@ -76,9 +82,9 @@ while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
 
-    cv2.polylines(img=frame, pts=[POLYGON_UP], isClosed=True, color=(255, 0, 255), thickness=4)
-    cv2.polylines(img=frame, pts=[POLYGON_DOWN], isClosed=True, color=(255, 255, 0), thickness=4)
-    cv2.polylines(img=frame, pts=[POLYGON_DOWN2], isClosed=True, color=(255, 255, 0), thickness=4)
+    cv2.polylines(img=frame, pts=[POLYGON_1], isClosed=True, color=(255, 0, 255), thickness=4)
+    cv2.polylines(img=frame, pts=[POLYGON_2], isClosed=True, color=(255, 255, 0), thickness=4)
+    # cv2.polylines(img=frame, pts=[POLYGON_2_2], isClosed=True, color=(255, 255, 0), thickness=4)
 
     # tm.start()
     if success:
@@ -107,16 +113,16 @@ while cap.isOpened():
                 # 判断行人是否跨越区域
                 is_crossing(x, y)
 
-        # d = (len(counter))
-        # u = (len(counter2))
-
-        cv2.putText(frame, ('goingdown: ') + str(len(counter)), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
-        cv2.putText(frame, ('goingup: ') + str(len(counter2)), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
+        cv2.putText(frame, 'IN: ' + str(len(counter_1)), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
+        # font.putText(frame, 'INPUT: ' + str(len(counter_1)), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
+        cv2.putText(frame, 'OUT: ' + str(len(counter_2)), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
+        # font.putText(frame, 'OUT: ' + str(len(counter_2)), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
 
         # Write the video frame
         output_video.write(frame)
 
         # Display the annotated frame
+        # frame = cv2.resize(frame, (960, 540))
         cv2.imshow("Tracking", frame)
 
         # Break the loop if 'q' is pressed
