ebaa638694665cc884553d3f6568a0f7881b1d2b
Hondlus <hldeshengyin@sina.com>
2024-11-07 16:27:01 +0800

    new commit

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import cv2
-from ultralytics import YOLO
+from ultralytics import YOLO, solutions
 from collections import defaultdict
 import numpy as np
 import matplotlib.path as mplPath
@@ -12,9 +12,9 @@ track_history = defaultdict(lambda: [])
 
 # Open the video file
 # video_path = "./test_video/test.mp4"
-# video_path = 0
+video_path = 0
 # video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/101" # 外走廊高清  1920 1080
-video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/102" # 外走廊标清 640 360
+# video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/102" # 外走廊标清 640 360
 # video_path = "rtsp://admin:HikNJQXFP@192.168.50.10:554/Streaming/Channels/102" # 屋内大屏摄像头
 # video_path = "rtsp://admin:Dxw202409@192.168.50.20:554/stream2"  # 15fps 640 480
 cap = cv2.VideoCapture(video_path)
@@ -27,29 +27,25 @@ output_video_path = "C:/Users/DXW/Desktop/yolo_track_reid/output_video/test.mp4"
 fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
 output_video = cv2.VideoWriter(output_video_path, fourcc, fps, (w, h), isColor=True)  # 创建一个VideoWriter对象用于写视频
 
-# vh_1 = []
-vh_1 = set()
-counter_1_list = []
-counter_1 = set()
-# vh_2 = []
-vh_2 = set()
-counter_2_list = []
-counter_2 = set()
+red_list = []
+blue_list = []
+red_region = set()
+blue_region = set()
 in_count = 0
 out_count = 0
 
 # 多边形坐标  高清 x:795、950 标清 x:260、320
-POLYGON_1 = np.array([
+RED_POLYGON = np.array([
     [0, 0], # 左上
-    [340, 0], # 右上
-    [340, 480], # 右下
+    [320, 0], # 右上
+    [320, 480], # 右下
     [0, 480], # 左下
 ])
-POLYGON_2 = np.array([
-    [350, 0], # 左上
+BLUE_POLYGON = np.array([
+    [330, 0], # 左上
     [640, 0], # 右上
     [640, 480], # 右下
-    [350, 480], # 左下
+    [330, 480], # 左下
 ])
 # POLYGON_2_2 = np.array([
 #     [325, 0],  # 左上
@@ -60,22 +56,35 @@ POLYGON_2 = np.array([
 
 # 是否穿越区域
 def is_crossing(xc, yc):
-    global in_count, out_count
-    #####going DOWN#####
-    # mplPath.Path(POLYGON_1).contains_point((xc, yc)判断中心点是否在区域内
-    if mplPath.Path(POLYGON_1).contains_point((xc, yc)):
-        vh_1.add(track_id)
-    if track_id in vh_1:
-        if mplPath.Path(POLYGON_2).contains_point((xc, yc)):
-            counter_2.add(track_id)
+    if mplPath.Path(RED_POLYGON).contains_point((xc, yc)):
+        red_region.add(track_id)
+    if mplPath.Path(BLUE_POLYGON).contains_point((xc, yc)):
+        blue_region.add(track_id)
+    return red_region, blue_region
 
-    #####going UP#####
-    if mplPath.Path(POLYGON_2).contains_point((xc, yc)):
-        vh_2.add(track_id)
-    if track_id in vh_2:
-        if mplPath.Path(POLYGON_1).contains_point((xc, yc)):
-            counter_1.add(track_id)
-
+    # global in_count, out_count
+    # #####going DOWN#####
+    # if mplPath.Path(RED_POLYGON).contains_point((xc, yc)):
+    #     red_region.add(track_id)
+    #     red_list.append(track_id)
+    #     if track_id in red_region and track_id not in blue_region:
+    #         pass
+    #     elif track_id in red_region and track_id in blue_region:
+    #         if red_list == [track_id]:
+    #             out_count += 1
+    #     else:
+    #         red_region.remove(track_id)
+    #
+    # if mplPath.Path(BLUE_POLYGON).contains_point((xc, yc)):
+    #     blue_region.add(track_id)
+    #     if track_id in blue_region and track_id not in red_region:
+    #         pass
+    #     elif track_id in blue_region and track_id in red_region:
+    #         if blue_list == [track_id]:
+    #             in_count += 1
+    #     else:
+    #         blue_region.remove(track_id)
+    #         blue_list.remove(track_id)
 
 # tm = cv2.TickMeter()
 # Loop through the video frames
@@ -107,17 +116,33 @@ while cap.isOpened():
                 points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
                 cv2.polylines(frame, [points], isClosed=False, color=(0, 255, 255), thickness=5)
 
-                # 判断行人是否跨越区域
-                is_crossing(x, y)
+                # 判断行人在哪个区域内，是否跨越区域
+                red_set, blue_set = is_crossing(x, y)
 
-        cv2.polylines(img=frame, pts=[POLYGON_1], isClosed=True, color=(255, 0, 255), thickness=4)
-        cv2.polylines(img=frame, pts=[POLYGON_2], isClosed=True, color=(255, 255, 0), thickness=4)
+            if track_id in red_set:
+                if track_id not in blue_set:
+                    pass
+                if track_id in blue_set:
+                    out_count += 1
+                    red_set.remove(track_id)
+
+            if track_id in blue_set:
+                if track_id not in red_set:
+                    pass
+                if track_id in red_set:
+                    in_count += 1
+                    blue_set.remove(track_id)
+
+
+        # Draw the polygon on the frame
+        cv2.polylines(img=frame, pts=[RED_POLYGON], isClosed=True, color=(255, 0, 255), thickness=4)
+        cv2.polylines(img=frame, pts=[BLUE_POLYGON], isClosed=True, color=(255, 255, 0), thickness=4)
         # cv2.polylines(img=frame, pts=[POLYGON_2_2], isClosed=True, color=(255, 255, 0), thickness=4)
 
-        cv2.putText(frame, 'IN: ' + str(len(counter_1)), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
-        # font.putText(frame, 'INPUT: ' + str(len(counter_1)), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
-        cv2.putText(frame, 'OUT: ' + str(len(counter_2)), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
-        # font.putText(frame, 'OUT: ' + str(len(counter_2)), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
+        # cv2.putText(frame, 'IN: ' + str(len(counter_1)), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
+        cv2.putText(frame, 'INPUT: ' + str(in_count), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
+        # cv2.putText(frame, 'OUT: ' + str(len(counter_2)), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
+        cv2.putText(frame, 'OUT: ' + str(out_count), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
 
         # Write the video frame
         output_video.write(frame)
