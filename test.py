b66aa87578f518a2dba3ad4e64c6125fc50c3967
Hondlus <hldeshengyin@sina.com>
2024-11-08 10:39:25 +0800

    beta1 modified commit

--- test.py
+++ test.py
@@ -12,9 +12,9 @@ track_history = defaultdict(lambda: [])
 
 # Open the video file
 # video_path = "./test_video/test.mp4"
-# video_path = 0
+video_path = 0
 # video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/101" # 外走廊高清  1920 1080
-video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/102" # 外走廊标清 640 360
+# video_path = "rtsp://admin:HikFIATCT@192.168.50.11:554/Streaming/Channels/102" # 外走廊标清 640 360
 # video_path = "rtsp://admin:HikNJQXFP@192.168.50.10:554/Streaming/Channels/102" # 屋内大屏摄像头
 # video_path = "rtsp://admin:Dxw202409@192.168.50.20:554/stream2"  # 15fps 640 480
 cap = cv2.VideoCapture(video_path)
@@ -36,15 +36,40 @@ out_count = 0
 RED_POLYGON = np.array([
     [0, 0], # 左上
     [320, 0], # 右上
-    [320, 480], # 右下
-    [0, 480], # 左下
+    [320, 360], # 右下
+    [0, 360], # 左下
 ])
 BLUE_POLYGON = np.array([
     [330, 0], # 左上
     [640, 0], # 右上
-    [640, 480], # 右下
-    [330, 480], # 左下
+    [640, 360], # 右下
+    [330, 360], # 左下
 ])
+
+def is_crossing(x_center, y_center):
+    global in_count, out_count
+    # 当区域人数达到一定数量，进行定量删除，节省内存，稳定性能
+    if len(red_region) > 10000:
+        red_region.pop(0)
+    if len(blue_region) > 10000:
+        blue_region.pop(0)
+
+    # 判断行人在哪个区域内，是否跨越区域
+    if mplPath.Path(RED_POLYGON).contains_point((x, y)):
+        red_region.add(track_id)
+        if track_id in red_region:
+            if track_id in blue_region:
+                in_count += 1
+                blue_region.remove(track_id)
+
+    if mplPath.Path(BLUE_POLYGON).contains_point((x, y)):
+        blue_region.add(track_id)
+        if track_id in blue_region:
+            if track_id in red_region:
+                out_count += 1
+                red_region.remove(track_id)
+
+    return in_count, out_count
 
 # tm = cv2.TickMeter()
 # Loop through the video frames
@@ -76,31 +101,14 @@ while cap.isOpened():
                 points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
                 cv2.polylines(frame, [points], isClosed=False, color=(0, 255, 255), thickness=5)
 
-                if len(red_region) > 10000:
-                    red_region.pop(0)
-                if len(blue_region) > 10000:
-                    blue_region.pop(0)
-
-                # 判断行人在哪个区域内，是否跨越区域
-                if mplPath.Path(RED_POLYGON).contains_point((x, y)):
-                    red_region.add(track_id)
-                    if track_id in red_region:
-                        if track_id in blue_region:
-                            in_count += 1
-                            blue_region.remove(track_id)
-
-                if mplPath.Path(BLUE_POLYGON).contains_point((x, y)):
-                    blue_region.add(track_id)
-                    if track_id in blue_region:
-                        if track_id in red_region:
-                            out_count += 1
-                            red_region.remove(track_id)
+                # 判断行人在哪个区域，是否跨越区域
+                in_count, out_count = is_crossing(x, y)
 
         # Draw the polygon on the frame
         cv2.polylines(img=frame, pts=[RED_POLYGON], isClosed=True, color=(255, 0, 255), thickness=4)
         cv2.polylines(img=frame, pts=[BLUE_POLYGON], isClosed=True, color=(255, 255, 0), thickness=4)
 
-        cv2.putText(frame, 'INPUT: ' + str(in_count), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
+        cv2.putText(frame, 'IN: ' + str(in_count), (20, 90), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0),2)
         cv2.putText(frame, 'OUT: ' + str(out_count), (20, 60), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
 
         # Write the video frame
